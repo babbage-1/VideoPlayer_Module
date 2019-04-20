cdc21fea3502306f1078961de418cbd817d1739d
asuluntas <aysunsulun@gmail.com>
2019-04-19 20:09:24 -0700

    seeds database

--- server/db/data/generate.js
+++ server/db/data/generate.js
@@ -12,11 +12,11 @@ var writeData = function(i) {
     var randomVideoIndex = faker.random.number({"min": 0, "max": 499});
     var randomVideoId = videoData.videos[randomVideoIndex].contentDetails.videoId;
     var url = `https://www.youtube.com/watch?v=${randomVideoId}`;
-    var associated1 = faker.random.number({"min": 0, "max": 499});
-    var associated2 = faker.random.number({"min": 0, "max": 499});
-    var associated3 = faker.random.number({"min": 0, "max": 499});
-    var associated4 = faker.random.number({"min": 0, "max": 499});
-    var associated5 = faker.random.number({"min": 0, "max": 499});
+    var associated1 = faker.random.number({"min": 1, "max": 10000000});
+    var associated2 = faker.random.number({"min": 1, "max": 10000000});
+    var associated3 = faker.random.number({"min": 1, "max": 10000000});
+    var associated4 = faker.random.number({"min": 1, "max": 10000000});
+    var associated5 = faker.random.number({"min": 1, "max": 10000000});
     var associatedVideosArray = `{${associated1}, ${associated2}, ${associated3}, ${associated4}, ${associated5}}`;
     csvString = `${name}|${url}|${associatedVideosArray}\n`
     data += csvString;
@@ -28,40 +28,4 @@ var writeData = function(i) {
 for (var i = 1; i <= 10; i++){
   writeData(i);
 }
-
-
-// var writeData = function(i) {
-//   var header = 'name|url|associatedVideos\n';
-//   var data = header;
-//   var csvString;
-//   for (var j = 1; j < 1000001; j++) {
-//     var name = faker.lorem.words(2);
-//     var videoId = j;
-//     var url = `https://www.youtube.com/watch?v=${videoId}`;
-//     var associated1 = faker.random.number({"min": 1, "max": 10000000});
-//     var associated2 = faker.random.number({"min": 1, "max": 10000000});
-//     var associated3 = faker.random.number({"min": 1, "max": 10000000});
-//     var associated4 = faker.random.number({"min": 1, "max": 10000000});
-//     var associated5 = faker.random.number({"min": 1, "max": 10000000});
-//     var associatedVideosArray = `[${associated1}, ${associated2}, ${associated3}, ${associated4}, ${associated5}]`;
-//     csvString = `${name}|${url}|${associatedVideosArray}\n`
-//     data += csvString;
-//   }
-//   fs.writeFileSync(`./data${i}.csv`, data);
-// }
-
-// for (var i = 1; i <= 10; i++){
-//   writeData(i);
-// }
-
-
-
-
-
-
-
-
-
-
-
 
--- server/db/data/seed.js
+++ server/db/data/seed.js
@@ -1,21 +1,101 @@
-\c videoplayer
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'postgres',
+  host: '127.0.0.1',
+  database: 'mydb',
+  password: '',
+  port: 5432,
+});
 
-CREATE TABLE videos (
-  name varchar(50),
-  url varchar(100),
-  associatedVideos int []
-);
 
-//TODO: add auto incremented id
+(async () => {
 
-\copy videos FROM 'server/db/data/csv/data1.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
-\copy videos FROM 'server/db/data/csv/data2.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
-\copy videos FROM 'server/db/data/csv/data3.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
-\copy videos FROM 'server/db/data/csv/data4.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
-\copy videos FROM 'server/db/data/csv/data5.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
-\copy videos FROM 'server/db/data/csv/data6.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
-\copy videos FROM 'server/db/data/csv/data7.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
-\copy videos FROM 'server/db/data/csv/data8.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
-\copy videos FROM 'server/db/data/csv/data9.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
-\copy videos FROM 'server/db/data/csv/data10.csv' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+  const client = await pool.connect()
+
+  try {
+    console.time('timing seed');
+
+    await client.query('BEGIN');
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS videos(
+        name VARCHAR(50) NOT NULL,
+        url VARCHAR(100) NOT NULL,
+        associatedVideos INT [] NOT NULL
+        );
+    `);
+
+    console.log('writing to database!');
+
+    const path1 = '/Users/aysun/Documents/hr/video-player-and-carousel/server/db/data/csv/data1.csv';
+    const path2 = '/Users/aysun/Documents/hr/video-player-and-carousel/server/db/data/csv/data2.csv';
+    const path3 = '/Users/aysun/Documents/hr/video-player-and-carousel/server/db/data/csv/data3.csv';
+    const path4 = '/Users/aysun/Documents/hr/video-player-and-carousel/server/db/data/csv/data4.csv';
+    const path5 = '/Users/aysun/Documents/hr/video-player-and-carousel/server/db/data/csv/data5.csv';
+    const path6 = '/Users/aysun/Documents/hr/video-player-and-carousel/server/db/data/csv/data6.csv';
+    const path7 = '/Users/aysun/Documents/hr/video-player-and-carousel/server/db/data/csv/data7.csv';
+    const path8 = '/Users/aysun/Documents/hr/video-player-and-carousel/server/db/data/csv/data8.csv';
+    const path9 = '/Users/aysun/Documents/hr/video-player-and-carousel/server/db/data/csv/data9.csv';
+    const path10 = '/Users/aysun/Documents/hr/video-player-and-carousel/server/db/data/csv/data10.csv';
+
+    await client.query(`
+    COPY videos FROM '${path1}' WITH (FORMAT CSV, HEADER, DELIMITER('|'))
+    `);
+
+    await client.query(`
+      COPY videos FROM '${path2}' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+    `);
+
+    await client.query(`
+      COPY videos FROM '${path3}' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+    `);
+
+    await client.query(`
+      COPY videos FROM '${path4}' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+    `);
+
+    await client.query(`
+      COPY videos FROM '${path5}' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+    `);
+
+    await client.query(`
+      COPY videos FROM '${path6}' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+    `);
+
+    await client.query(`
+      COPY videos FROM '${path7}' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+    `);
+    await client.query(`
+      COPY videos FROM '${path8}' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+    `);
+
+    await client.query(`
+      COPY videos FROM '${path9}' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+    `);
+
+    await client.query(`
+      COPY videos FROM '${path10}' WITH (FORMAT CSV, HEADER, DELIMITER('|'));
+    `);
+
+    await client.query(`
+      ALTER TABLE videos ADD COLUMN id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY;
+    `);
+
+    await client.query('COMMIT');
+
+    console.timeEnd('timing seed');
+
+  } catch (e) {
+    await client.query('ROLLBACK');
+    console.log('error!');
+    throw e;
+  } finally {
+    client.release();
+  }
+})().catch(e => console.error(e.stack));
+
+
+
+
+
+
 
